038bfb5eb9c0ada4cf972045e391314019e3ae23
sarapapi <57095209+sarapapi@users.noreply.github.com>
2025-03-29 12:29:32 +0000

    Deploying to gh-pages from @ sarapapi/sarapapi.github.io@b14771ba1096be37eceb44d56291e0c9fd65d195 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,21 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-",
+          title: '',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/2024_ACL_outstanding/";
+            },},{id: "news-",
+          title: '',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024_EMNLP_award/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-successfully-defended-my-phd",
+          title: 'I successfully defended my PhD 🎊',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
